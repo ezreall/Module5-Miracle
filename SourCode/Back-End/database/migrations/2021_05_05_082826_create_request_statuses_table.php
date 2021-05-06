df201c5885dd70275c51b3ab72098d109ede4446
hoang-van-hung <justforyou96hd@gmail.com>
2021-05-06 17:48:58 +0700

    update data

--- SourCode/Back-End/database/migrations/2021_05_05_082826_create_request_statuses_table.php
+++ SourCode/Back-End/database/migrations/2021_05_05_082826_create_request_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRentersTable extends Migration
+class CreateRequestStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateRentersTable extends Migration
      */
     public function up()
     {
-        Schema::create('renters', function (Blueprint $table) {
+        Schema::create('request_statuses', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('provider_id');
-            $table->unsignedBigInteger('user_id');
             $table->string('name');
-            $table->string('address');
-            $table->foreign('provider_id')->references('id')->on('providers');
-            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateRentersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('renters');
+        Schema::dropIfExists('requests_statuses');
     }
 }
--- SourCode/Back-End/database/seeders/RequestStatusesSeeder.php
+++ SourCode/Back-End/database/seeders/RequestStatusesSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\RenterStatus;
+use App\Models\RequestStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
-class RenterStatusSeederTable extends Seeder
+class RequestStatusesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,16 +15,17 @@ class RenterStatusSeederTable extends Seeder
      */
     public function run()
     {
-        $status = new RenterStatus();
-        $status->name="Processing";
+        $status = new RequestStatus();
+        $status->name = "processing";
         $status->save();
 
-        $status = new RenterStatus();
-        $status->name="Cancel";
+        $status = new RequestStatus();
+        $status->name = "approve";
         $status->save();
 
-        $status = new RenterStatus();
-        $status->name="Approved";
+        $status = new RequestStatus();
+        $status->name = "cancle";
         $status->save();
+
     }
 }
